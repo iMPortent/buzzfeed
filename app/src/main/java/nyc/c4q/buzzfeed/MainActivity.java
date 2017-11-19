51fa578f7f2e989bb8dd2012886c734b75715076
Taishawn Williams <twill4317@withs.org>
2017-11-18 19:01:35 -0500

    first commit

---
+++ app/src/main/java/nyc/c4q/buzzfeed/MainActivity.java
@@ -0,0 +1,25 @@
+package nyc.c4q.buzzfeed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Quiz>myList = new ArrayList();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.quiz_layout);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(manager);
+
+
+    }
+}
---
+++ app/src/main/java/nyc/c4q/buzzfeed/Quiz.java
@@ -0,0 +1,9 @@
+package nyc.c4q.buzzfeed;
+
+/**
+ * Created by MarckemX on 11/18/17.
+ */
+
+public class Quiz {
+
+}
